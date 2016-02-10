0fd020a8546583c61c8d4574e3ddc0bae6730f40
stan wielga <svwielga4@gmail.com>
2016-02-10 15:09:26 -0800

    added classes for x and o

--- animation.js
+++ animation.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-    $('.player_1_stat').click()
+    $('.player_1_stat  button').click(function(){
+        $('.x-test').addClass('x');
+    });
 
-    $('.player_2_stat').click()
+    $('.player_2_stat  button').click(function(){
+        $('.o-test').addClass('o');
+    })
 });
