030b82bdd1e04e499871675e113c342c660db8bd
Jaison Amos <jaisonamos@gmail.com>
2016-02-10 11:47:23 -0800

    Html skeleton created

---
+++ script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jaisonamos on 2/10/16.
+ */
+
+$(document).ready(function () {
+    $(".back").click(function () {
+        console.log(this);
+        (this);
+
+    });
+
+
+
+});
